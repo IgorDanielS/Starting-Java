4f891c01c5555ac8ac8da66511558fc396fc4131
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2023-05-12 19:00:34 -0300

    Add files via upload

---
+++ DateSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datesystem;
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class DateSystem {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.print("A hora do sistema é: ");
+        System.out.print(relogio.toString());
+    }
+}
