78009820375679aec5682f4ecb7db31370c106ff
Igor Daniel <72262654+IgorDanielS@users.noreply.github.com>
2023-05-13 06:14:28 -0300

    Add files via upload
    curso em video - desafio 1

---
+++ ScreenResolutionChallenge.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.datesystem;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+/**
+ *
+ * @author Admin
+ */
+public class ScreenResolutionChallenge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("Screen width = " + d.width);
+        System.out.println("Screen height = " + d.height);
+    }
+    
+}
